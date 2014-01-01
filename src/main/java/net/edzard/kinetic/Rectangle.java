33b3ab19d001251766bc95e95e7f62eceb237784
Jiří Smolík <kukurka@gmail.com>
2014-01-01 21:37:54 +0100

    Methods update.
    1) Some methods were moved up in the node hierarchy.
    2) Removed the old and incompatible "transitionTo" methods.
    3) Added a convenience method to check intersection with another
    rectangle.

--- src/main/java/net/edzard/kinetic/Rectangle.java
+++ src/main/java/net/edzard/kinetic/Rectangle.java
@@ -14,38 +14,6 @@ public class Rectangle extends Shape {
 	}
 	
 	/**
-	 * Retrieve the rectangle shape width.
-	 * @return The width
-	 */
-	public final native double getWidth() /*-{
-		return this.getWidth();
-	}-*/;
-
-	/**
-	 * Assign the rectangle shape width.
-	 * @param width A width value
-	 */
-	public final native void setWidth(double width) /*-{
-		this.setWidth(width);
-	}-*/;
-
-	/**
-	 * Retrieve the rectangle shape height.
-	 * @return The width
-	 */
-	public final native double getHeight() /*-{
-		return this.getHeight();
-	}-*/;
-
-	/**
-	 * Assign the rectangle shape height.
-	 * @param height A height value
-	 */
-	public final native void setHeight(double height) /*-{
-		this.setHeight(height);
-	}-*/;
-	
-	/**
 	 * Retrieve the rectangle shape corner radius.
 	 * @return The corner radius
 	 */
@@ -61,30 +29,20 @@ public class Rectangle extends Shape {
 	public final native void setCornerRadius(double radius) /*-{
 		this.setCornerRadius(radius);
 	}-*/;
-	 
-	/**
-	 * Animate a linear transition of this rectangle shape.
-	 * @param target Another rectangle shape - defines the characteristics that the current rectangle shape will have at the end of the animation
-	 * @param duration The time it will take for the animation to complete, in seconds
-	 * @return An object for controlling the transition.
-	 */
-	public final Transition transitionTo(Rectangle target, double duration) {
-		return transitionTo(target, duration, null, null);
-	}
 	
 	/**
-	 * Animate a transition of this rectangle shape.
-	 * @param target Another rectangle shape - defines the characteristics that the current rectangle shape will have at the end of the animation
-	 * @param duration The time it will take for the animation to complete, in seconds
-	 * @param ease An easing function that defines how the transition will take place
-	 * @param callback A function that will be called at the end of the animation
-	 * @return An object for controlling the transition.
+	 * Does this rectangle intersect with another?
+	 * Copied and assimilated from StackOverflow answer #2: http://stackoverflow.com/questions/2752349/fast-rectangle-to-rectangle-intersection
+	 * @param other The other rectangle
 	 */
-	public final Transition transitionTo(Rectangle target, double duration, EasingFunction ease, Runnable callback) {
-		StringBuffer sb = new StringBuffer();
-		if (this.getWidth() != target.getWidth()) sb.append("width:").append(target.getWidth()).append(",");
-		if (this.getHeight() != target.getHeight()) sb.append("height:").append(target.getHeight()).append(",");
-		if (this.getCornerRadius() != target.getCornerRadius()) sb.append("cornerRadius:").append(target.getCornerRadius()).append(",");
-		return transitionToShape(target, sb, duration, ease, callback);
+	public final boolean intersects(Vector2d otherRectAbsPos, Vector2d otherRectSize)
+	{
+		Vector2d absPosA = getAbsolutePosition();
+		Vector2d sizeA = getSize();
+		return (absPosA.x <= (otherRectAbsPos.x + otherRectSize.x) && // a.left <= b.right
+				otherRectAbsPos.x <= (absPosA.x + sizeA.x) && // b.left <= a.right
+				absPosA.y <= (otherRectAbsPos.y + otherRectSize.y) && // a.top <= b.bottom
+				otherRectAbsPos.y <= (absPosA.y + sizeA.y) // b.top <= a.bottom
+		);
 	}
 }
